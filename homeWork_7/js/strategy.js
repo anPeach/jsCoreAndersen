d7f043c5c11b7a38aa6cc7ed8ed2bb1d0ca6dc26
anPeach <nastyapisarenko2001@gmail.com>
2021-10-20 19:07:51 +0300

    Fix operations chain mistake

--- homeWork_7/js/strategy.js
+++ homeWork_7/js/strategy.js
@@ -1,31 +1,83 @@
-import { sum, sub, mul, div } from './utils.js';
+import {
+  canClearInput,
+  canOperate,
+  canSwitchOperation,
+  div,
+  getResult,
+  isPrevResult,
+  mul,
+  setOptions,
+  sub,
+  sum,
+} from './utils.js';
 
 const input = document.querySelector('.calculator__screen');
 
-const setInfo = (operation) => {
-  info.operation = operation;
-  info.first = input.value;
-  input.value = '';
+const enteredValues = [];
+const lastEnteredValue = {
+  entered: false,
 };
 
-const info = {
-  operation: null,
-  first: null,
-  current: null,
-  result: null,
+const setInfo = (operation) => {
+  if (enteredValues[length - 1] === operation) {
+    return;
+  }
+  if (enteredValues.length !== 1) {
+    enteredValues.push(Number(input.value));
+  }
+  enteredValues.push(operation);
+  input.value = getResult(enteredValues);
+  lastEnteredValue.entered = false;
 };
 
 const operationsTypes = {
+  operation: null,
+  lastValue: null,
   sum: () => {
+    if (canSwitchOperation(enteredValues, sum) && !lastEnteredValue.entered) {
+      enteredValues[enteredValues.length - 1] = sum;
+      return;
+    }
+
+    if (!canOperate(enteredValues, lastEnteredValue.entered)) {
+      return;
+    }
     setInfo(sum);
   },
   mul: () => {
+    if (canSwitchOperation(enteredValues, mul) && !lastEnteredValue.entered) {
+      enteredValues[enteredValues.length - 1] = mul;
+      return;
+    }
+
+    if (!canOperate(enteredValues, lastEnteredValue.entered)) {
+      return;
+    }
+
     setInfo(mul);
   },
   sub: () => {
+    if (canSwitchOperation(enteredValues, sub) && !lastEnteredValue.entered) {
+      enteredValues[enteredValues.length - 1] = sub;
+      return;
+    }
+
+    if (!canOperate(enteredValues, lastEnteredValue.entered)) {
+      return;
+    }
+
     setInfo(sub);
   },
   div: () => {
+    if (canSwitchOperation(enteredValues, div) && !lastEnteredValue.entered) {
+      enteredValues[enteredValues.length - 1] = div;
+      return;
+    }
+
+    if (!canOperate(enteredValues, lastEnteredValue.entered)) {
+      return;
+    }
+
     setInfo(div);
   },
   switchSign: () => {
@@ -34,44 +86,49 @@ const operationsTypes = {
     }
 
     input.value *= -1;
-    info.current = input.value;
+
+    if (isPrevResult(enteredValues)) {
+      enteredValues.length = 0;
+      enteredValues.push(Number(input.value));
+    }
   },
-  equal: () => {
-    info.result = info.operation(Number(info.first), Number(info.current));
-    input.value = info.result;
-    info.first = info.result;
+  equal: function () {
+    setOptions(enteredValues, this, Number(input.value));
+
+    const result = getResult(enteredValues);
+
+    enteredValues.length = 0;
+    enteredValues.push(result);
+    input.value = result;
+    lastEnteredValue.entered = false;
   },
 };
 
 const buttonsTypes = {
   number: (target) => {
-    if (info.result !== null) {
-      info.result = null;
+    if (canClearInput(enteredValues, input.value, lastEnteredValue.entered)) {
       input.value = '';
     }
 
-    if (input.value === 'Error') {
-      input.value = '';
+    if (isPrevResult(enteredValues)) {
+      enteredValues.length = 0;
     }
 
     input.value += target.value;
-    info.current = input.value;
+    lastEnteredValue.entered = true;
   },
   mathOperation: (target) => {
     operationsTypes[target.dataset.mathOperation]();
   },
   clear: () => {
     input.value = '';
-    info.operation = null;
-    info.current = null;
-    info.first = null;
-    info.result = null;
+    enteredValues.length = 0;
   },
   dot: () => {
     input.value += '.';
   },
   clearOneSign: () => {
-    if (info.operation !== null && info.result === Number(input.value)) {
+    if (isPrevResult(enteredValues)) {
       return;
     }
 
--- homeWork_7/js/utils.js
+++ homeWork_7/js/utils.js
@@ -1,16 +1,73 @@
-const roundToEight = (value) => {
-    return Math.round(value * 100000000) / 100000000;
-}
+const roundToEight = (value) => Math.round(value * 100000000) / 100000000;
 
-const sum = (firstVal, secondVal) => firstVal + secondVal;
-const mul = (firstVal, secondVal) => firstVal * secondVal;
-const sub = (firstVal, secondVal) => firstVal - secondVal;
-const div=(firstVal, secondVal) =>{
-    if(secondVal === 0) {
-        throw new Error('attempt to divide by 0');
+const lastIsOperation = (arr) => typeof arr[arr.length - 1] === 'function' && arr.length > 0;
+
+const sum = (firstVal, secondVal) => roundToEight(firstVal + secondVal);
+const mul = (firstVal, secondVal) => roundToEight(firstVal * secondVal);
+const sub = (firstVal, secondVal) => roundToEight(firstVal - secondVal);
+const div = (firstVal, secondVal) => {
+  if (secondVal === 0) {
+    throw new Error('attempt to divide by 0');
+  }
+
+  return roundToEight(firstVal / secondVal);
+};
+
+const getResult = (arr) => {
+  return arr.reduce((acc, item, index) => {
+    if (index === 0) {
+      acc = item;
+      return acc;
+    }
+    if (typeof item === 'number') {
+      acc = arr[index - 1](acc, Number(item));
     }
 
-    return roundToEight(firstVal / secondVal);
-}
+    return acc;
+  }, 0);
+};
 
-export {div, mul, sub, sum}
+const canOperate = (arr, entered) => !(lastIsOperation(arr) && entered === false);
+
+const isPrevResult = (arr) => arr.length === 1;
+
+const setOptions = (arr, options, value) => {
+  if (isPrevResult(arr)) {
+    arr.push(options.operation);
+    arr.push(options.lastValue);
+    return;
+  }
+  options.operation = arr[arr.length - 1];
+  options.lastValue = value;
+  arr.push(value);
+};
+
+const canClearInput = (arr, value, entered) => {
+  const lastElem = arr[arr.length - 1];
+
+  if (value === 'Error') {
+    return true;
+  }
+
+  if ((typeof lastElem !== 'number' || arr.length === 1) && entered === false) {
+    return true;
+  }
+
+  return false;
+};
+
+const canSwitchOperation = (arr, func) => arr[arr.length - 1] !== func && arr.length > 1;
+
+export {
+  canClearInput,
+  canOperate,
+  canSwitchOperation,
+  div,
+  getResult,
+  isPrevResult,
+  lastIsOperation,
+  mul,
+  setOptions,
+  sub,
+  sum,
+};
